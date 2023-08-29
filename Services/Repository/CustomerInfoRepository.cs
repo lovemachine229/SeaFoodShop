927bc085bc55e2be0aa8ca65353ed4a5fac75049
hola <lavanhoa229@gmail.com>
2023-08-29 17:12:56 +0700

    29/8

---
+++ Services/Repository/CustomerInfoRepository.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Repository
+{
+    public interface ICustomerInfoRepository
+    {
+        int Create(CustomerInfor obj);
+    }
+
+    public class CustomerInfoRepository : RepositoryBase, ICustomerInfoRepository
+    {
+        public CustomerInfoRepository(IDbTransaction transaction) : base(transaction)
+        {
+        }
+
+        public int Create(CustomerInfor obj)
+        {
+            try
+            {
+                var p = new DynamicParameters();
+
+                p.Add("@GuidId", obj.GuidId);
+                p.Add("@PhoneNo", obj.PhoneNo);
+                p.Add("@Address", obj.Address);
+                p.Add("@FullName", obj.FullName);
+
+                return ExecuteStoredProcedure("CustomerInfo_Create", p);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Services/Repository/UtilityRepository.cs
@@ -0,0 +1,104 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Repository
+{
+    public interface IUtilityRepository
+    {
+        int CountProduct();
+
+        int CountCustomer();
+
+        int CountOrder();
+
+        decimal CountRevenue();
+    }
+
+    public class UtilityRepository : RepositoryBase, IUtilityRepository
+    {
+        public UtilityRepository(IDbTransaction transaction) : base(transaction)
+        {
+        }
+
+        public int CountCustomer()
+        {
+            try
+            {
+                var p = new DynamicParameters();
+
+                p.Add("@Output", dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                ExecuteStoredProcedure("Utility_CountCustomer", p);
+
+                return p.Get<int>("@Output");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public int CountOrder()
+        {
+            try
+            {
+                var p = new DynamicParameters();
+
+                p.Add("@Output", dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                ExecuteStoredProcedure("Utility_CountOrder", p);
+
+                return p.Get<int>("@Output");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public int CountProduct()
+        {
+            try
+            {
+                var p = new DynamicParameters();
+
+                p.Add("@Output", dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                ExecuteStoredProcedure("Utility_CountProduct", p);
+
+                return p.Get<int>("@Output");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public decimal CountRevenue()
+        {
+            try
+            {
+                var p = new DynamicParameters();
+
+                p.Add("@Output", dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                ExecuteStoredProcedure("Utility_CountRevenue", p);
+
+                return p.Get<int>("@Output");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
